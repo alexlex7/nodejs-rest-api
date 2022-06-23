925e1e42096af7f7833a114609e3b7ee02256748
alexlex7 <alekslosev7@gmail.com>
2022-06-23 16:28:03 +0300

    connected to mongoDB

--- server.js
+++ server.js
@@ -1,5 +1,25 @@
-const app = require('./app')
+const app = require('./app');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+mongoose.Promise = global.Promise;
+
+mongoose
+  .connect(process.env.DB_HOST, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(3000, () => {
+      console.log('database connected, server running on port 3000');
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
+// app.listen(3000, () => {
+//   console.log('Server running. Use our API on port: 3000');
+// });
+
+// passwor zz2wOgupzAiydCPg
+// mongodb+srv://alexlex7:zz2wOgupzAiydCPg@cluster0.0rtkc.mongodb.net/test
